3c1c7a6fc7e831e39b989c3c0a3ac438f7caecd2
Matt Wheeler <wheelermt@proton.me>
2025-03-07 14:09:13 +0000

    Complete exercises 1-3

--- exercises/03_ticket_v1/02_validation/src/lib.rs
+++ exercises/03_ticket_v1/02_validation/src/lib.rs
@@ -18,7 +18,27 @@ impl Ticket {
     // as well as some `String` methods. Use the documentation of Rust's standard library
     // to find the most appropriate options -> https://doc.rust-lang.org/std/string/struct.String.html
     fn new(title: String, description: String, status: String) -> Self {
-        todo!();
+        // Validate status
+        match status.as_str() {
+            "To-Do" | "In Progress" | "Done" => {}
+            _ => panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed"),
+        }
+
+        if description.is_empty() {
+            panic!("Description cannot be empty")
+        }
+
+        if title.is_empty() {
+            panic!("Title cannot be empty")
+        }
+
+        if title.len() > 50 {
+            panic!("Title cannot be longer than 50 bytes")
+        }
+
+        if description.len() > 500 {
+            panic!("Description cannot be longer than 500 bytes")
+        }
         Self {
             title,
             description,
@@ -26,7 +46,6 @@ impl Ticket {
         }
     }
 }
-
 #[cfg(test)]
 mod tests {
     use super::*;
